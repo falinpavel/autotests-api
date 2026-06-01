0caa174dcf04f047ef0e7cb0888a8f183d7e671f
falinpavel <falinpavel96@gmail.com>
2026-06-02 00:55:29 +0300

    5.5 Знакомство с gRPC протоколом. Практика работы с gRPC протоколом

--- grpc_client.py
+++ grpc_client.py
@@ -7,5 +7,5 @@ import user_service_pb2
 channel = grpc.insecure_channel('localhost:50051')
 stub = user_service_pb2_grpc.UserServiceStub(channel)
 
-responses = stub.GetUser(user_service_pb2.GetUserRequest(username="John"))
-print(responses)
+response = stub.GetUser(user_service_pb2.GetUserRequest(username="John"))
+print(response)

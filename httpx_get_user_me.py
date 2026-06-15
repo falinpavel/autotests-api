099f08f79d3337e00a9065c8c56af329a8f3bf1d
falinpavel <falinpavel96@gmail.com>
2026-06-15 22:47:13 +0300

    6.1 Введение в HTTPX. Практическое задание: работа с HTTPX

--- httpx_get_user_me.py
+++ httpx_get_user_me.py
@@ -1,18 +1,18 @@
-# import httpx
-#
-# # Тело запроса POST /authentication/login, в email и password передаем данные своего пользователя
-# login_payload = {
-#     "email": "123@yandex.ru",
-#     "password": "2556535",
-# }
-# # Выполняем запрос, передаем свой payload в json
-# login_response = httpx.post(url="http://localhost:8000/api/v1/authentication/login", json=login_payload)
-#
-# # Сохраняем accessToken в переменную
-# access_token = login_response.json()["token"]["accessToken"]
-#
-# # Выполняем запрос GET /users/me, используя ранее полученный accessToken в ответе метода POST /authentication/login
-# with httpx.Client(headers={"Authorization": f"Bearer {access_token}"}) as client:
-#     users_me_response = client.get(url="http://localhost:8000/api/v1/users/me")
-#     print(f"Статус код ответа: {users_me_response.status_code}")
-#     print(f"Тело ответа: {users_me_response.json()}")
+import httpx
+
+# Тело запроса POST /authentication/login, в email и password передаем данные своего пользователя
+login_payload = {
+    "email": "123@yandex.ru",
+    "password": "2556535",
+}
+# Выполняем запрос, передаем свой payload в json
+login_response = httpx.post(url="http://localhost:8000/api/v1/authentication/login", json=login_payload)
+
+# Сохраняем accessToken в переменную
+access_token = login_response.json()["token"]["accessToken"]
+
+# Выполняем запрос GET /users/me, используя ранее полученный accessToken в ответе метода POST /authentication/login
+with httpx.Client(headers={"Authorization": f"Bearer {access_token}"}) as client:
+    users_me_response = client.get(url="http://localhost:8000/api/v1/users/me")
+    print(f"Статус код ответа: {users_me_response.status_code}")
+    print(f"Тело ответа: {users_me_response.json()}")

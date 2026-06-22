d18d73c12544ebd1789ac374bb06472e5d9f3ccc
falinpavel <falinpavel96@gmail.com>
2026-06-22 23:12:47 +0300

    6.4 Практикуемся в реализации API клиентов. Практика реализации API клиентов: /api/v1/courses.

---
+++ clients/courses/courses_client.py
@@ -0,0 +1,97 @@
+from typing import TypedDict
+from httpx import Response
+
+from clients.api_client import APIClient
+
+
+class GetCoursesQueryDict(TypedDict):
+    """
+    Описание структуры запроса на получение списка курсов текущего пользователя.
+    """
+    userId: str
+
+
+class CreateCourseRequestDict(TypedDict):
+    """
+    Описание структуры запроса на создание курса.
+    """
+    title: str
+    maxScore: int | None
+    minScore: int | None
+    description: str
+    estimatedTime: str | None
+    previewFileId: str
+    createdByUserId: str
+
+
+class UpdateCourseRequestDict(TypedDict):
+    """
+    Описание структуры запроса на обновление курса.
+    """
+    title: str | None
+    maxScore: int | None
+    minScore: int | None
+    description: str | None
+    estimatedTime: str | None
+
+
+class CoursesClient(APIClient):
+    """
+    Клиент для работы с /api/v1/courses
+    """
+
+    def get_courses_api(self, query: GetCoursesQueryDict) -> Response:
+        """
+        Метод получения списка курсов.
+
+        :param query: Словарь с userId.
+        :return: Ответ от сервера в виде объекта httpx.Response
+        """
+        return self.get(
+            url="/api/v1/courses",
+            params=query
+        )
+
+    def get_course_api(self, course_id: str) -> Response:
+        """
+        Метод получения курса.
+
+        :param course_id: Идентификатор курса.
+        :return: Ответ от сервера в виде объекта httpx.Response
+        """
+        return self.get(url=f"/api/v1/courses/{course_id}")
+
+    def create_course_api(self, request: CreateCourseRequestDict) -> Response:
+        """
+        Метод создания курса.
+
+        :param request: Словарь с title, maxScore, minScore, description, estimatedTime,
+        previewFileId, createdByUserId.
+        :return: Ответ от сервера в виде объекта httpx.Response
+        """
+        return self.post(
+            url="/api/v1/courses",
+            data=request
+        )
+
+    def update_course_api(self, course_id: str, request: UpdateCourseRequestDict) -> Response:
+        """
+        Метод обновления курса.
+
+        :param course_id: Идентификатор курса.
+        :param request: Словарь с title, maxScore, minScore, description, estimatedTime.
+        :return: Ответ от сервера в виде объекта httpx.Response
+        """
+        return self.patch(
+            url=f"/api/v1/courses/{course_id}",
+            json=request
+        )
+
+    def delete_course_api(self, course_id: str) -> Response:
+        """
+        Метод удаления курса.
+
+        :param course_id: Идентификатор курса.
+        :return: Ответ от сервера в виде объекта httpx.Response
+        """
+        return self.delete(url=f"/api/v1/courses/{course_id}")

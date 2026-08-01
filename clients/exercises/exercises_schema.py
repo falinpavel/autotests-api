66d471affd672224689761c12a16d663264e46f5
falinpavel <falinpavel96@gmail.com>
2026-08-01 23:04:10 +0300

    7.4 Знакомство с Faker. Практика работы с генерацией случайных данных

--- clients/exercises/exercises_schema.py
+++ clients/exercises/exercises_schema.py
@@ -48,7 +48,7 @@ class UpdateExerciseRequestSchema(BaseModel):
     title: str | None = Field(default_factory=fake.sentence)
     max_score: int | None = Field(alias="maxScore", default_factory=fake.max_score)
     min_score: int | None = Field(alias="minScore", default_factory=fake.min_score)
-    order_index: int | None = Field(alias="orderIndex")
+    order_index: int | None = Field(alias="orderIndex", default_factory=fake.random_integer)
     description: str | None = Field(default_factory=fake.text)
     estimated_time: str | None = Field(alias="estimatedTime", default_factory=fake.estimated_time)
 

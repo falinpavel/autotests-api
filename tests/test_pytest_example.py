eceb5ad3d07f37cada8a00b95a120421af137644
falinpavel <falinpavel96@gmail.com>
2026-08-04 15:57:02 +0300

    8.2 Написание API автотестов используя Pytest

--- tests/test_pytest_example.py
+++ tests/test_pytest_example.py
@@ -1,12 +1,12 @@
-
-
-class TestExample:
-
-    def test_create_user(self):
-        pass
-
-    def test_login(self):
-        pass
-
-    def test_logout(self):
-        pass
+#
+#
+# class TestExample:
+#
+#     def test_create_user(self):
+#         pass
+#
+#     def test_login(self):
+#         pass
+#
+#     def test_logout(self):
+#         pass
---
+++ tools/assertions/base.py
@@ -0,0 +1,31 @@
+from typing import Any
+
+
+def assert_status_code(actual_status_code: int, expected_status_code: int) -> None:
+    """
+    Проверяет, что фактический статус-код ответа соответствует ожидаемому.
+
+    :param actual_status_code: Фактический статус-код ответа.
+    :param expected_status_code: Ожидаемый статус-код.
+    :raises AssertionError: Если статус-коды не совпадают.
+    """
+    assert actual_status_code == expected_status_code, (
+        f"Incorrect status code. "
+        f"Actual status code: {actual_status_code} "
+        f"Expected status code: {expected_status_code}"
+    )
+
+def assert_equal(actual: Any, expected: Any, name: str) -> None:
+    """
+    Проверяет, что фактическое значение равно ожидаемому.
+
+    :param name: Название проверяемого значения.
+    :param actual: Фактическое значение.
+    :param expected: Ожидаемое значение.
+    :raises AssertionError: Если фактическое значение не равно ожидаемому.
+    """
+    assert actual == expected, (
+        f"Incorrect value in: {name}. "
+        f"Actual value: {actual} "
+        f"Expected value: {expected}"
+    )

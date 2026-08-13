0fa64b75c4ad6adf6b7b158393255049bce43734
falinpavel <falinpavel96@gmail.com>
2026-08-13 21:46:24 +0300

    8.3 Маркировки Pytest. Практика работы с pytest маркировками

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,4 +1,5 @@
 from http import HTTPStatus
+import pytest
 
 from clients.users.public_users_client import get_public_users_client
 from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema
@@ -7,11 +8,14 @@ from tools.assertions.base import assert_status_code
 from tools.assertions.users import assert_create_user_response
 
 
+@pytest.mark.users
+@pytest.mark.regression
 def test_create_user():
     public_users_client = get_public_users_client()
     request = CreateUserRequestSchema()
     response = public_users_client.create_user_api(request)
     response_data = CreateUserResponseSchema.model_validate_json(json_data=response.text)
+
     assert_status_code(actual_status_code=response.status_code, expected_status_code=HTTPStatus.OK)
     assert_create_user_response(request=request, response=response_data)
     validate_json_schema(instance=response.json(), schema=response_data.model_json_schema())
